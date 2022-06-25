7957d36fde85551aaabb85770e95e4cb73a08bac
Sergey Zelyukin <sergey.zelyukin@gmail.com>
2022-06-25 06:49:08 +0000

    parameters as options

--- cellsworld/cellsworld.py
+++ cellsworld/cellsworld.py
@@ -14,10 +14,10 @@ DEFAULT_HEIGHT = 10
 
 
 @click.command()
-@click.argument("width", type=int, default=DEFAULT_WIDTH)
-@click.argument("height", type=int, default=DEFAULT_HEIGHT)
-@click.argument("frames_count", type=int, default=DEFAULT_FRAMES_COUNT)
-@click.argument("frame_rate", type=float, default=DEFAULT_FRAME_RATE)
+@click.option("--width", type=int, default=DEFAULT_WIDTH)
+@click.option("--height", type=int, default=DEFAULT_HEIGHT)
+@click.option("--frames_count", type=int, default=DEFAULT_FRAMES_COUNT)
+@click.option("--frame_rate", type=float, default=DEFAULT_FRAME_RATE)
 def start(width, height, frames_count, frame_rate):
     canvas = Canvas(width, height)
     canvas.add_cells(
